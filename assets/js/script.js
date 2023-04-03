6e9f97cb1f0a6d6f55a6590c1e7d78a04bd330c1
Janna Harmon <harmonjanna00@gmail.com>
2023-04-03 00:28:42 -0400

    Added some JavaScript code and added comments to the HTML and CSS files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,56 @@
-// Call startQuiz function when the "Start Quiz" button is clicked
-function startQuiz(); {
-    addEventListener("click");
-}
+// Allows the quiz to start when the "Start Quiz" button is clicked
+document.getElementById("start-quiz").addEventListener("click", quizQuestions);
+
+// Make an object called quizQuestions to set up the quiz
+const quizQuestions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "The condition of an if/else statement is enclosed with:",
+        answers: {
+            1: "quotes",
+            2: "curly brackets",
+            3: "parenthesis",
+            4: "square brackets"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above"
+        },
+        correctAnswer: "4"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables:",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis"
+        },
+        correctAnswer: "3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console.log",
+        },
+        correctAnswer: "4"
+    },
+];
